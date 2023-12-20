b97a7910861276e7b4823bedcf3a7c2abe1fb0b2
BogdanSavvy <bogdansavin315@gmail.com>
2023-12-20 17:19:33 +0200

    finishing with catalog page

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+'use strict';
+
+//? Ініціалізація слайдера-свайпера
 const swiper = new Swiper('.swiper', {
 	pagination: {
 		el: '.swiper-pagination',
@@ -6,6 +9,29 @@ const swiper = new Swiper('.swiper', {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
+	loop: true,
+	autoplay: {
+		delay: 7000,
+	},
 	speed: 400,
 	spaceBetween: 100,
+});
+
+//? Інтерактивний drop-down для сортування
+const ddTrigger = document.querySelector('.custom-select__top');
+const ddSelect = document.querySelector('.custom-select');
+
+ddTrigger.addEventListener('click', () => {
+	console.log('click');
+	ddSelect.classList.toggle('custom-select--open');
+});
+
+//? Інтерактивні drop-down фільтри
+const filterTriggers = document.querySelectorAll('.catalog-filter__top');
+const filterMenu = document.querySelectorAll('.catalog-filter');
+
+filterTriggers.forEach((trigger, index) => {
+	trigger.addEventListener('click', () => {
+		filterMenu[index].classList.toggle('catalog-filter--open');
+	});
 });
