8a121100789eb9bb7637dd78fc54e61b18b77302
BogdanSavvy <bogdansavin315@gmail.com>
2023-12-28 13:04:22 +0200

    Some fixes

--- src/js/main.js
+++ src/js/main.js
@@ -18,13 +18,16 @@ const swiper = new Swiper('.swiper', {
 });
 
 const mainSwiper = new Swiper('.swiper-main', {
-	effect: "coverflow",
+	effect: 'coverflow',
 	pagination: {
 		el: '.swiper-pagination',
 	},
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
+	},
+	autoplay: {
+		delay: 7000,
 	},
 	loop: true,
 	speed: 400,
@@ -100,96 +103,98 @@ if (triggerBurger) {
 		navSidebar.classList.toggle('nav--visible');
 	});
 }
-
 if (closeBurgerButton) {
 	closeBurgerButton.addEventListener('click', () => {
 		navSidebar.classList.remove('nav--visible');
 	});
 }
 
-//catalog-grid__content
-//data-grid-columns
-//catalog-columns__btn
-//catalog-columns__btn--current
-
 const catalogColumnsButtons = document.querySelectorAll(
 	'.catalog-columns__btn',
 );
 const catalogGridContent = document.querySelector('.catalog-grid__content');
 
-catalogColumnsButtons.forEach(button => {
-	button.addEventListener('click', event => {
-		event.currentTarget.classList.add('catalog-columns__btn--current');
+if (catalogColumnsButtons || catalogGridContent) {
+	catalogColumnsButtons.forEach(button => {
+		button.addEventListener('click', event => {
+			event.currentTarget.classList.add('catalog-columns__btn--current');
 
-		let ButonDataAttributeValue =
-			event.currentTarget.getAttribute('data-columns');
+			let ButonDataAttributeValue =
+				event.currentTarget.getAttribute('data-columns');
 
-		catalogGridContent.setAttribute(
-			'data-grid-columns',
-			ButonDataAttributeValue,
-		);
+			catalogGridContent.setAttribute(
+				'data-grid-columns',
+				ButonDataAttributeValue,
+			);
+		});
 	});
-});
+}
 
 //? Динамічний інпут
 const input = document.getElementById('input');
 const stepPlusButton = document.querySelector('.stepper__btn--plus');
 const stepMinusButton = document.querySelector('.stepper__btn--minus');
 
-stepPlusButton.addEventListener('click', () => {
-	if (input.value < 5) {
-		stepPlusButton.classList.remove('stepper__btn--disabled');
-		input.value = +input.value + 1;
-	} else {
-		input.value = +input.value + 0;
-	}
-});
+if (stepPlusButton || stepMinusButton) {
+	stepPlusButton.addEventListener('click', () => {
+		if (input.value < 5) {
+			stepPlusButton.classList.remove('stepper__btn--disabled');
+			input.value = +input.value + 1;
+		} else {
+			input.value = +input.value + 0;
+		}
+	});
 
-if (input.value >= 1) {
-	stepMinusButton.classList.remove('stepper__btn--disabled');
-}
-
-stepMinusButton.addEventListener('click', () => {
 	if (input.value >= 1) {
-		input.value = +input.value - 1;
-	} else {
-		input.value = +input.value - 0;
+		stepMinusButton.classList.remove('stepper__btn--disabled');
 	}
-});
+
+	stepMinusButton.addEventListener('click', () => {
+		if (input.value >= 1) {
+			input.value = +input.value - 1;
+		} else {
+			input.value = +input.value - 0;
+		}
+	});
+}
 
 //? Перемикання кольрів для зміни контенту
 const colorSelectButtons = document.querySelectorAll('.color-select__btn');
 const cardSliders = document.querySelectorAll('.card-slider');
 
-colorSelectButtons.forEach(button => {
-	button.addEventListener('click', e => {
-		let hasActiveClass = button.classList.contains('color-select__btn--active');
-		if (e.target.getAttribute('data-color') === 'white') {
-			for (let cardSlider of cardSliders) {
-				if (cardSlider.getAttribute('data-color') === 'white') {
-					cardSlider.classList.remove('visually-hidden');
-				} else {
-					cardSlider.classList.add('visually-hidden');
+if (colorSelectButtons || cardSliders) {
+	colorSelectButtons.forEach(button => {
+		button.addEventListener('click', e => {
+			let hasActiveClass = button.classList.contains(
+				'color-select__btn--active',
+			);
+			if (e.target.getAttribute('data-color') === 'white') {
+				for (let cardSlider of cardSliders) {
+					if (cardSlider.getAttribute('data-color') === 'white') {
+						cardSlider.classList.remove('visually-hidden');
+					} else {
+						cardSlider.classList.add('visually-hidden');
+					}
 				}
 			}
-		}
-		if (e.target.getAttribute('data-color') === 'black') {
-			for (let cardSlider of cardSliders) {
-				if (cardSlider.getAttribute('data-color') === 'black') {
-					cardSlider.classList.remove('visually-hidden');
-				} else {
-					cardSlider.classList.add('visually-hidden');
+			if (e.target.getAttribute('data-color') === 'black') {
+				for (let cardSlider of cardSliders) {
+					if (cardSlider.getAttribute('data-color') === 'black') {
+						cardSlider.classList.remove('visually-hidden');
+					} else {
+						cardSlider.classList.add('visually-hidden');
+					}
 				}
 			}
-		}
 
-		if (hasActiveClass) {
-			e.target.classList.remove('color-select__btn--active');
-		} else {
-			button.classList.add('color-select__btn--active');
-		}
+			if (hasActiveClass) {
+				e.target.classList.remove('color-select__btn--active');
+			} else {
+				button.classList.add('color-select__btn--active');
+			}
+		});
 	});
-});
+}
 
 //? Перемикання між описом та характеристиками товару
 document.addEventListener('DOMContentLoaded', () => {
@@ -226,3 +231,17 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 });
+
+//? Плавний скрол вгору
+const scrollToTopButton = document.querySelector('.to-top');
+
+if (scrollToTopButton) {
+	scrollToTopButton.addEventListener('click', e => {
+		e.preventDefault();
+
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
+	});
+}
